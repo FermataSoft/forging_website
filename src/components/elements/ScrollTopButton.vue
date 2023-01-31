984c0b4a3583e429a7baf030a30f6a00d4f79e69
Fermata <olegandreev1997@mail.ru>
2023-01-31 14:36:07 +0300

    Add "scroll to top" button functionality

--- src/components/elements/ScrollTopButton.vue
+++ src/components/elements/ScrollTopButton.vue
@@ -1,13 +1,44 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const windowHeight = ref(window.innerHeight);
+const scrollYPos = ref(window.scrollY);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', () => {
+    scrollYPos.value = window.scrollY;
+  });
+
+  window.addEventListener('resize', () => {
+    windowHeight.value = window.innerHeight;
+  });
+});
+</script>
 
 <template>
-  <div class="circle"></div>
+  <Transition>
+    <div class="btn-scrollTop" v-show="scrollYPos > windowHeight" @click="scrollToTop"></div>
+  </Transition>
 </template>
 
 <style lang="scss" scoped>
 @import '../../assets/_vars.scss';
 
-.circle {
+span {
+  position: fixed;
+  bottom: 100px;
+  right: 100px;
+}
+
+.btn-scrollTop {
+  z-index: 9999;
   position: fixed;
   bottom: 30px;
   right: 30px;
@@ -15,19 +46,21 @@
   width: 50px;
   height: 50px;
   border-radius: 50%;
-  border: 4px solid $navbar;
+  mix-blend-mode: difference;
 
   cursor: pointer;
 
-  &::before, &::after {
-    content: "";
+  &::before,
+  &::after {
+    content: '';
     position: absolute;
     top: 50%;
     left: 50%;
     height: 5px;
     width: 20px;
-    background-color: $navbar;
     border-radius: 2px;
+    background-color: $navbar;
+    filter: invert(1);
   }
 
   &::before {
@@ -37,5 +70,26 @@
   &::after {
     transform: translate(-20%, -50%) rotate(45deg);
   }
+
+  &:hover {
+    mix-blend-mode: normal;
+  }
+
+  &:hover::before,
+  &:hover::after {
+    background-color: $accent;
+    filter: invert(0);
+  }
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+  bottom: 0px;
+  opacity: 0;
 }
 </style>
