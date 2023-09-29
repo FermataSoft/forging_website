83c0b2aae7a846f6f41ebc5eb4b975413ec9a52c
Oleg Andreev <subneck@gmail.com>
2023-09-29 14:28:48 +0300

    Change category by route query

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,39 +6,36 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: () => import('../views/HomeView.vue'),
     },
     {
       path: '/works',
       name: 'works',
-      component: ()=> import('../views/WorksView.vue')
-    },
-    {
-      path: '/works/:category',
-      name: 'works-category',
-      component: ()=> import('../views/WorksView.vue')
+      component: () => import('../views/WorksView.vue')
     },
     {
       path: '/articles',
       name: 'articles',
-      component: ()=> import('../views/ArticlesView.vue')
+      component: () => import('../views/ArticlesView.vue')
     },
     {
       path: '/contacts',
       name: 'contacts',
-      component: ()=> import('../views/ContactsView.vue')
+      component: () => import('../views/ContactsView.vue')
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: '404',
+      component: () => import('../404.vue')
     }
   ],
-  scrollBehavior (to, from, savedPosition) {
-    return { top: 0}
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 }
   }
 })
 
