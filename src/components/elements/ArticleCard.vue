601f62c2f99266da6ea8150a8503d17e11519583
Oleg Andreev Desktop <test@test.test>
2023-10-09 22:52:03 +0300

    Articles page init

---
+++ src/components/elements/ArticleCard.vue
@@ -0,0 +1,86 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  text: String,
+  imagePath: String
+})
+</script>
+
+<template>
+  <router-link class="article-card" to="/">
+    <div class="article-card__image">
+      <img src="../../wrapper_images/1000x1000.jpg" alt="">
+    </div>
+
+    <div class="article-card__text-block">
+      <h1 class="article-card__title">{{ title }}</h1>
+      <p class="article-card__description">{{ text }}</p>
+    </div>
+  </router-link>
+</template>
+
+<style lang="scss" scoped>
+@import '../../assets/_vars.scss';
+
+.article-card {
+  width: 100%;
+  background-color: $surface-container-lowest;
+  padding: 20px 30px;
+  display: flex;
+  flex-direction: row;
+  gap: 50px;
+  transition: all 0.3s;
+  color: $on-surface;
+  position: relative;
+  overflow: hidden;
+
+  &:hover {
+    background-color: $surface;
+    box-shadow: 0 5px 10px 5px #e0e0e0;
+
+    &:after {
+      transform: translate(50%, -50%);
+    } 
+  }
+
+  &:after {
+    content: "";
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    right: 0;
+    top: 50%;
+    border-radius: 50%;
+    transform: translate(100%, -50%);
+    transition: all 0.3s 0.3s ease-in-out;
+    
+    background-color: $primary;
+  }
+}
+
+.article-card__image {
+  height: 200px;
+  overflow: hidden;
+  flex-shrink: 0;
+
+  img {
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+.article-card__text-block {
+  flex-grow: 1;
+}
+
+.article-card__title {
+  font-size: 1.6rem;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.article-card__description {
+  font-size: 1.4rem;
+  text-align: justify;
+}
+</style>
--- src/views/ArticlesView.vue
+++ src/views/ArticlesView.vue
@@ -1,9 +1,58 @@
-<script></script>
+<script setup>
+import { useI18n } from 'vue-i18n';
+import SectionHeader from '../components/elements/SectionHeader.vue';
+import ArticleCard from '../components/elements/ArticleCard.vue'
+
+const { t } = useI18n()
+</script>
 
 <template>
   <div class="wrapper">
-    <h1>Articles View:)</h1>
+    <SectionHeader>{{ t("ArticlesHeader") }}</SectionHeader>
+
+    <div class="articles">
+        <ArticleCard :title="'gsdfgfsd'"
+          :text="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae felis leo. Nunc viverra lacus urna. Vestibulum accumsan convallis quam non dictum. Maecenas lacinia ultricies convallis. Duis sagittis lorem vitae erat facilisis, ac mollis sapien finibus. Vestibulum ultricies consectetur porta. Ut tempus lorem ac turpis tempor, eu malesuada nibh aliquam. Ut egestas magna vel erat efficitur finibus. Nullam et lorem a odio lobortis rutrum sed ac tortor. Donec accumsan venenatis diam, quis viverra turpis rhoncus vel. Morbi pulvinar eget mi ut pretium.'">
+        </ArticleCard>
+
+      <ArticleCard :title="'gsdfgfsd'"
+        :text="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae felis leo. Nunc viverra lacus urna. Vestibulum accumsan convallis quam non dictum. Maecenas lacinia ultricies convallis. Duis sagittis lorem vitae erat facilisis, ac mollis sapien finibus. Vestibulum ultricies consectetur porta. Ut tempus lorem ac turpis tempor, eu malesuada nibh aliquam. Ut egestas magna vel erat efficitur finibus. Nullam et lorem a odio lobortis rutrum sed ac tortor. Donec accumsan venenatis diam, quis viverra turpis rhoncus vel. Morbi pulvinar eget mi ut pretium.'">
+      </ArticleCard>
+      <ArticleCard :title="'gsdfgfsd'"
+        :text="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae felis leo. Nunc viverra lacus urna. Vestibulum accumsan convallis quam non dictum. Maecenas lacinia ultricies convallis. Duis sagittis lorem vitae erat facilisis, ac mollis sapien finibus. Vestibulum ultricies consectetur porta. Ut tempus lorem ac turpis tempor, eu malesuada nibh aliquam. Ut egestas magna vel erat efficitur finibus. Nullam et lorem a odio lobortis rutrum sed ac tortor. Donec accumsan venenatis diam, quis viverra turpis rhoncus vel. Morbi pulvinar eget mi ut pretium.'">
+      </ArticleCard>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+@import '../assets/_vars.scss';
+
+.wrapper {
+  margin-bottom: 50px;
+}
+
+.articles {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 80%;
+}
+
+.articles__item {
+
+
+}
+</style>
+
+<i18n>
+  {
+    "ru-RU": {
+      ArticlesHeader: "Статьи"
+    },
+    "by-BY": {
+      ArticlesHeader: "Артыкул"
+    }
+  }
+</i18n>
