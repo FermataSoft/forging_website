1c9fee124183e4fa3d1ef606f92962d351f5539a
Oleg Andreev / Desktop <olegandreev1997@mail.ru>
2024-02-05 09:33:40 +0300

    Usage of API to get content from DB

--- src/views/ArticlesView.vue
+++ src/views/ArticlesView.vue
@@ -4,41 +4,16 @@ import { useRoute } from "vue-router";
 import { useI18n } from "vue-i18n";
 import SectionHeader from "../components/elements/SectionHeader.vue";
 import ArticleCard from "../components/elements/ArticleCard.vue";
-import initSqlJs from "sql.js";
+
+import { Database } from "../api/db";
 
 const { t } = useI18n();
 const content = ref([]);
-
-async function initDB() {
-  const sqlPromise = initSqlJs({
-    locateFile: (file) =>
-      "https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.wasm",
-  });
-
-  const dataPromise = fetch("../database.db").then((res) => res.arrayBuffer());
-  const [SQL, buf] = await Promise.all([sqlPromise, dataPromise]);
-  const db = new SQL.Database(new Uint8Array(buf));
-
-  const contentDb = db.exec(`SELECT * FROM articles`);
-
-  return contentDb[0];
-}
-
-function contentToObj(contentDB) {
-  let contentObj = {};
-
-  contentDB.values.forEach((value) => {
-    contentDB.columns.forEach((columnName, index) => {
-      contentObj[columnName] = value[index];
-    });
-    content.value.push(contentObj);
-    contentObj = {};
-  });
-}
+const db = new Database();
 
 onBeforeMount(() => {
-  initDB().then((result) => {
-    contentToObj(result);
+  db.getContent("SELECT * FROM articles;").then((result) => {
+    content.value = db.contentToObj(result);
   });
 });
 </script>
