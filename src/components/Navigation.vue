1fc7c2d0d1cbede6430f432a4ab93c64f3027ef8
Oleg Andreev / Desktop <olegandreev1997@mail.ru>
2024-02-05 09:31:23 +0300

    Burger menu was developed on mobile resolutions

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,68 +1,113 @@
 <script setup>
-import { ref } from 'vue';
-import { onMounted, onUnmounted } from 'vue';
-import OrderButton from './elements/OrderButton.vue';
-import LanguageDropdownMenu from './LanguageDropdownMenu.vue';
+import { ref, onMounted, onUnmounted, watch } from "vue";
+import OrderButton from "./elements/OrderButton.vue";
+import LanguageDropdownMenu from "./LanguageDropdownMenu.vue";
 
-let scrollPos = ref(0);
-let scrollTrigger = 80;
+const isBurgerMenuActive = ref(false);
+const windowWidth = ref(window.innerWidth);
+const scrollPos = ref(0);
+const scrollTrigger = 80;
+const scrollbarWidth = ref();
 
 onMounted(() => {
-  window.addEventListener('scroll', scrollHandler)
+  window.addEventListener("scroll", scrollHandler);
+  scrollbarWidth.value = window.innerWidth - document.documentElement.clientWidth
 });
 
 onUnmounted(() => {
-  window.removeEventListener('scroll', scrollHandler)
-})
+  window.removeEventListener("scroll", scrollHandler);
+});
 
 function scrollHandler() {
   scrollPos.value = window.scrollY;
 }
+
+function toggleState(state) {
+  return state ? false : true;
+}
+
+watch(isBurgerMenuActive, (newVal, oldVal) => {
+  if (newVal && windowWidth.value < 820) {
+    document.body.style.paddingRight = `${scrollbarWidth.value}px`;
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.paddingRight = "0px";
+    document.body.style.overflow = "auto";
+  }
+});
 </script>
 
 <template>
-  <header class="navbar" :class="{ 'navbar--scrolled': $route.path != '/' || scrollPos > scrollTrigger }">
-    <div class="wrapper">
+  <header
+    class="navbar"
+    :class="{
+      'navbar--scrolled': $route.path != '/' || scrollPos > scrollTrigger,
+      'navbar__wrapper--mobile-active': isBurgerMenuActive,
+    }"
+  >
+    <div class="navbar__wrapper">
       <div class="logo-wrapper">
         <img class="logo" src="../favicon/android-chrome-512x512.png" alt="" />
       </div>
 
       <nav>
         <div class="nav__item">
-          <RouterLink to="/">Главная</RouterLink>
+          <RouterLink to="/" @click="isBurgerMenuActive = false"
+            >Главная</RouterLink
+          >
         </div>
         <div class="nav__item">
-          <RouterLink :to="{path: '/works', query: {category: 'all'}}">Наши работы</RouterLink>
+          <RouterLink
+            :to="{ path: '/works', query: { category: 'all' } }"
+            @click="isBurgerMenuActive = false"
+            >Наши работы</RouterLink
+          >
         </div>
         <div class="nav__item">
-          <RouterLink to="/articles">Статьи</RouterLink>
+          <RouterLink to="/articles" @click="isBurgerMenuActive = false"
+            >Статьи</RouterLink
+          >
         </div>
         <div class="nav__item">
-          <RouterLink to="/contacts">Контакты</RouterLink>
+          <RouterLink to="/contacts" @click="isBurgerMenuActive = false"
+            >Контакты</RouterLink
+          >
         </div>
         <div class="nav__item">
-          <RouterLink to="/about">О нас</RouterLink>
+          <RouterLink to="/about" @click="isBurgerMenuActive = false"
+            >О нас</RouterLink
+          >
         </div>
       </nav>
       <OrderButton>Заказать</OrderButton>
-      <LanguageDropdownMenu></LanguageDropdownMenu>
+      <LanguageDropdownMenu
+        class="navbar__language-dropdown-menu"
+      ></LanguageDropdownMenu>
+    </div>
+    <div
+      class="navbar__burger-menu"
+      :class="{ 'navbar__burger-menu--active': isBurgerMenuActive }"
+      @click="() => (isBurgerMenuActive = toggleState(isBurgerMenuActive))"
+    >
+      <span></span>
     </div>
   </header>
 </template>
 
 <style lang="scss" scoped>
-@import '../assets/_vars.scss';
+@import "../assets/_vars.scss";
 
 .navbar {
   position: fixed;
   display: block;
   height: $navbar-height;
   width: 100%;
-  z-index: 9999999;
-  transition: all .2s;
+  z-index: 5;
+  transition: all 0.2s;
   background-color: transparent;
 
-  .wrapper {
+  .navbar__wrapper {
+    position: relative;
     max-width: $wrapper-width;
     height: 100%;
     margin: auto;
@@ -70,8 +115,18 @@ function scrollHandler() {
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: space-between;
     gap: 60px;
+
+    transition: all 0.3s ease;
+
+    @include breakpoint(md) {
+      max-width: 100vw;
+      flex-direction: column;
+      height: 100vh;
+      background-color: $inverse-surface;
+      padding: 20px;
+      transform: translateY(-100%);
+    }
 
     .logo-wrapper {
       width: fit-content;
@@ -92,9 +147,14 @@ function scrollHandler() {
 
       display: flex;
       flex-direction: row;
-      gap: 30px;
+      gap: 20px;
       align-items: center;
       justify-content: center;
+
+      @include breakpoint(md) {
+        flex-direction: column;
+        height: auto;
+      }
 
       .nav__item {
         height: 100%;
@@ -113,8 +173,12 @@ function scrollHandler() {
 
         transition: color 0.2s ease-in-out;
 
+        @include breakpoint(md) {
+          font-size: 2rem;
+        }
+
         &::after {
-          content: '';
+          content: "";
 
           position: absolute;
           bottom: 0;
@@ -125,16 +189,17 @@ function scrollHandler() {
           transform: translate(-50%, 0%);
 
           background-color: $primary;
+
+          @include breakpoint(md) {
+            bottom: -7px;
+          }
         }
 
-        &::before,
         &::after {
           transition: all 0.3s ease-in-out;
         }
 
         &:hover {
-
-          &::before,
           &::after {
             width: 80%;
           }
@@ -156,7 +221,92 @@ function scrollHandler() {
   }
 }
 
+@include breakpoint(md) {
+  .navbar__burger-menu {
+    display: block;
+    position: absolute;
+    right: 20px;
+    top: 15px;
+    width: 25px;
+    height: 20px;
+    z-index: 4;
+    cursor: pointer;
+
+    &::before,
+    &::after {
+      content: "";
+      background-color: #fff;
+      position: absolute;
+      width: 100%;
+      height: 2px;
+      border-radius: 1px;
+      -webkit-transition: all 0.3s ease;
+      transition: all 0.3s ease;
+    }
+
+    &::before {
+      top: 0;
+      left: 0px;
+    }
+
+    &::after {
+      bottom: 0;
+      left: 0px;
+    }
+
+    span {
+      background-color: #fffdfb;
+      position: absolute;
+      width: 100%;
+      height: 2px;
+      top: 9px;
+      right: 0;
+      border-radius: 2px;
+      -webkit-transition: all 0.1s ease;
+      transition: all 0.1s ease;
+    }
+  }
+
+  .navbar__burger-menu--active {
+    &::before {
+      -webkit-transform: rotate(-45deg);
+      transform: rotate(-45deg);
+      top: 9px;
+      height: 2px;
+      left: 0;
+    }
+
+    &::after {
+      -webkit-transform: rotate(45deg);
+      transform: rotate(45deg);
+      top: 9px;
+      height: 2px;
+      left: 0;
+    }
+
+    span {
+      opacity: 0;
+      right: -10px;
+    }
+  }
+}
+
+.navbar__language-dropdown-menu {
+  @include breakpoint(md) {
+    position: absolute;
+    top: 0;
+    left: 20px;
+  }
+}
+
 .navbar--scrolled {
   background-color: $inverse-surface;
+  box-shadow: 0px 2px 10px hsla(0, 0%, 0%, 0.2);
+}
+
+.navbar__wrapper--mobile-active .navbar__wrapper {
+  @include breakpoint(md) {
+    transform: translateY(0%);
+  }
 }
 </style>
