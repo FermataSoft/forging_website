7be27d0ffac8bbce90fbf0d874cb527a8446cab9
Oleg Andreev / Desktop <olegandreev1997@mail.ru>
2024-04-23 11:04:22 +0300

    Add try/catch wrapper

--- public/database.php
+++ public/database.php
@@ -1,31 +1,30 @@
 <?php
+try {
+  $dbName = "interiorsteel";
+  $user = "root";
+  $password = "root";
+  // $connect = new PDO("mysql:host=localhost;dbname=***REMOVED***", "***REMOVED***", "***REMOVED***");
+  $connect = new PDO("mysql:host=mysql;dbname={$dbName}", $user, $password);
 
-$dbName = "interriorsteel.images";
-$user = "root";
-$password = "root";
-// $connect = new PDO("mysql:host=localhost;dbname=***REMOVED***", "***REMOVED***", "***REMOVED***");
-$connect = new PDO("mysql:host=localhost;dbname={$dbName}", $user, $password);
+  $received_data = json_decode(file_get_contents("php://input"), true);
 
-$received_data = json_decode(file_get_contents("php://input"), true);
+  $result = [];
 
-$result = [];
+  if ($_GET['action'] === 'fetch-all') {
 
-if ($received_data->action === 'fetch-all' || $_GET['action'] === 'fetch-all') {
+    $query = $_GET['query'] ?? "SELECT *
+              FROM images
+              ORDER BY id DESC";
 
-  if ($received_data->query) {
-    $query = $received_data->query;
-  } else {
-    $query = "SELECT *
-            FROM images
-            ORDER BY id DESC";
+    $statement = $connect->query($query);
+
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+
+      $result[] = $row;
+    }
+
+    echo json_encode($result);
   }
-
-  $statement = $connect->query($query);
-
-  while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-
-    $result[] = $row;
-  }
-
-  echo json_encode($result);
+} catch (PDOException $e) {
+  echo "Database error: " . $e->getMessage();
 }
