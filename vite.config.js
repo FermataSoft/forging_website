e40793429b1c5f52d83c5c815ad5aed827fccf16
Oleg Laptop <olegandreev1997@mail.ru>
2024-06-24 14:09:33 +0300

    Add autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue';
 import { resolve, dirname } from 'node:path'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,6 +27,13 @@ export default defineConfig({
     }),
     nodePolyfills(),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({})
+      ]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
