8cf8debc6fa0913d1fd6d95d61c468e30f382d25
Oleg Andreev <olegandreev1997@mail.ru>
2024-07-21 14:58:36 +0300

    Change API of fetching data from DB

--- public/api/articles.php
+++ public/api/articles.php
@@ -3,28 +3,30 @@ try {
   $dbName = "interiorsteel";
   $user = "root";
   $password = "root";
-  // $connect = new PDO("mysql:host=localhost;dbname=subneckgm2", "subneckgm2", "25Legansubn");
   $connect = new PDO("mysql:host=mysql;dbname={$dbName};charset=utf8", $user, $password);
-
   $received_data = json_decode(file_get_contents("php://input"), true);
-
   $result = [];
 
-  if ($_GET['action'] === 'fetch-all') {
-
-    $query = $_GET['query'] ?? "SELECT *
+  if (!empty($_GET['action']) && $_GET['action'] === 'fetch-all') {
+    $query = "SELECT *
               FROM articles
               ORDER BY id DESC";
-
-    $statement = $connect->query($query);
-
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-
-      $result[] = $row;
-    }
-
-    echo json_encode($result);
   }
+
+  if (empty($_GET['query'])) {
+    echo 'Request error: query parameter "query" is empty!';
+  } else {
+    $query = $_GET['query'];
+  }
+
+  $statement = $connect->query($query);
+
+  while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+
+    $result[] = $row;
+  }
+
+  echo json_encode($result);
 } catch (PDOException $e) {
   echo "Database error: " . $e->getMessage();
 }
