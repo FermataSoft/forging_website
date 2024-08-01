8df259208f2401710d07837a09f5614ca47135af
Oleg Andreev <olegandreev1997@mail.ru>
2024-08-01 17:00:23 +0300

    Add interface skeleton animation while fetching data from DB

--- src/components/elements/ArticleCard.vue
+++ src/components/elements/ArticleCard.vue
@@ -1,9 +1,16 @@
 <script setup>
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+
 const props = defineProps({
+  isLoaded: Boolean,
   id: Number,
   title: String,
   description: String,
   uploadDate: Number,
+  previewImage: String,
 });
 
 const dateOptions = {
@@ -12,24 +19,42 @@ const dateOptions = {
   day: "numeric",
 };
 
-const formattedUploadDate = new Date(props.uploadDate).toLocaleString(
-  "ru",
-  dateOptions
-);
-</script> 
+const formattedUploadDate = computed(() => {
+  return new Date(props.uploadDate).toLocaleString("ru", dateOptions);
+});
+</script>
 
 <template>
-  <router-link class="article-card" :to="`/articles/${id}`">
-    <div class="article-card__image">
-      <img src="/wrapper_images/1000x1000.jpg" alt="" />
-    </div>
+  <router-link :to="`/articles/${id}`">
+    <div class="article-card" :class="{ '--skeleton-active': !isLoaded }">
+      <div class="article-card__image" :class="{ '--skeleton': !isLoaded }">
+        <img
+          v-if="previewImage"
+          height="200px"
+          width="200px"
+          :src="'/images/articles/' + previewImage"
+          :alt="previewImage"
+        />
+        <span class="article-card__image-warning" v-else-if="isLoaded">{{
+          t("article-card__image-warning")
+        }}</span>
+      </div>
 
-    <div class="article-card__text-block">
-      <h1 class="article-card__title">{{ title }}</h1>
-      <p class="article-card__description">{{ description }}</p>
-    </div>
+      <div class="article-card__text-block">
+        <h1 class="article-card__title" :class="{ '--skeleton': !isLoaded }">
+          {{ title }}
+        </h1>
+        <p
+          class="article-card__description"
+          :class="{ '--skeleton': !isLoaded }"
+        >
+          {{ description }}
+        </p>
+      </div>
 
-    <span class="article-card__upload-date">{{ formattedUploadDate }}</span>
+      <div class="article-card__upload-date --skeleton" v-if="!isLoaded"></div>
+      <span class="article-card__upload-date" v-else>{{ formattedUploadDate }} </span>
+    </div>
   </router-link>
 </template>
 
@@ -78,23 +103,45 @@ const formattedUploadDate = new Date(props.uploadDate).toLocaleString(
     border-radius: 50%;
     transform: translate(100%, -50%);
     transition: all 0.3s 0.1s ease-in-out;
-
     background-color: $primary;
+  }
+
+  &.--skeleton-active {
+    background-color: #e7e7e7;
+    box-shadow: none;
   }
 }
 
 .article-card__image {
   height: 200px;
+  width: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   overflow: hidden;
   flex-shrink: 0;
+  text-align: center;
 
   @include breakpoint(sm) {
     height: 60px;
   }
 
   img {
-    height: 100%;
+    height: 200px;
+    width: 200px;
     object-fit: cover;
+  }
+
+  .article-card__image-warning {
+    font-size: 2rem;
+
+    @include breakpoint(sm) {
+      font-size: 1.6rem;
+    }
+  }
+
+  &.--skeleton {
+    @include skeleton(0);
   }
 }
 
@@ -111,6 +158,11 @@ const formattedUploadDate = new Date(props.uploadDate).toLocaleString(
     font-size: $header3-mobile;
     font-weight: $font-medium;
   }
+
+  &.--skeleton {
+    height: 30px;
+    @include skeleton;
+  }
 }
 
 .article-card__description {
@@ -120,18 +172,39 @@ const formattedUploadDate = new Date(props.uploadDate).toLocaleString(
   @include breakpoint(sm) {
     display: none;
   }
+
+  &.--skeleton {
+    height: 120px;
+    @include skeleton;
+  }
 }
 
 .article-card__upload-date {
   position: absolute;
   bottom: 20px;
   right: 20px;
-
   font-size: 1.2rem;
 
   @include breakpoint(sm) {
     left: 10px;
-    bottom: 10px
+    bottom: 10px;
+  }
+
+  &.--skeleton {
+    height: 15px;
+    width: 80px;
+    @include skeleton;
   }
 }
 </style>
+
+<i18n>
+{
+  ru-RU: {
+    "article-card__image-warning": "Изображение отсутствует",
+},
+  by-BY: {
+    "article-card__image-warning": "Малюнак адсутнічае",
+  }
+}
+</i18n>
--- src/views/ArticlesView.vue
+++ src/views/ArticlesView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onBeforeMount } from "vue";
+import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { useI18n } from "vue-i18n";
 import { useFetch } from "@vueuse/core";
@@ -7,18 +7,24 @@ import SectionHeader from "../components/elements/SectionHeader.vue";
 import ArticleCard from "../components/elements/ArticleCard.vue";
 
 const { t } = useI18n();
-const content = ref([]);
+const content = ref([{}, {}, {}]);
+let isLoaded = ref(false);
+let isError = ref(false);
 
-onBeforeMount(async () => {
+onMounted(async () => {
   const { isFetching, error, data } = await useFetch(
-    "/api/articles.php?query=SELECT id, title, description, creation_date FROM articles",
-    {
-      refetch: true,
-    }
+    "/api/articles.php?query=SELECT id, title, description, creation_date FROM articles"
   )
     .get()
     .json();
-  content.value = data.value;
+
+  if (error.value) {
+    isError.value = error.value;
+    throw new Error(error.value);
+  } else {
+    content.value = data.value;
+    isLoaded.value = !isFetching.value;
+  }
 });
 </script>
 
@@ -26,14 +32,16 @@ onBeforeMount(async () => {
   <div class="articles">
     <div class="articles__wrapper" v-if="useRoute().path === '/articles'">
       <SectionHeader>{{ t("ArticlesHeader") }}</SectionHeader>
-
+      
       <div class="articles__items">
         <ArticleCard
           v-for="item in content"
+          :is-loaded="isLoaded"
           :id="item.id"
           :title="item.title"
           :description="item.description"
           :upload-date="item.creation_date"
+          :preview-image="item.preview_image"
         >
         </ArticleCard>
       </div>
