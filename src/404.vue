389b0df19fd7c17243fba65390298bee0291bd18
Oleg Andreev <olegandreev1997@mail.ru>
2024-08-03 22:36:17 +0300

    Adaptive design of the page "404" for mobile

--- src/404.vue
+++ src/404.vue
@@ -1,36 +1,52 @@
 <script setup>
-import ButtonLink from './components/elements/ButtonLink.vue'
-import { useI18n } from 'vue-i18n';
+import ButtonLink from "./components/elements/ButtonLink.vue";
+import { useI18n } from "vue-i18n";
 const { t } = useI18n();
 </script>
 
 <template>
-  <main>
-    <h1>404</h1>
-    <h2>{{t("header_PageNotFound")}}</h2>
-    <ButtonLink class="link-home" link="/" text="ButtonGoHomePage"></ButtonLink>
+  <main class="error-page">
+    <h1 class="error-page__error-code">404</h1>
+    <h2 class="error-page__error-text">{{ t("header_PageNotFound") }}</h2>
+    <ButtonLink
+      class="error-page__link-home"
+      link="/"
+      text="ButtonGoHomePage"
+    ></ButtonLink>
   </main>
 </template>
 
 <style lang="scss" scoped>
-main {
+@import "@/assets/_vars.scss";
+
+.error-page {
   display: flex;
   height: 100vh;
+  height: 100dvh;
   width: 100%;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  text-align: center;
 
-  h1 {
+  .error-page__error-code {
     font-size: 16rem;
     font-weight: bold;
+
+    @include breakpoint(md) {
+      font-size: 10rem;
+    }
   }
 
-  h2 {
+  .error-page__error-text {
     font-size: 5rem;
+
+    @include breakpoint(md) {
+      font-size: 2.5rem;
+    }
   }
 
-  .link-home {
+  .error-page__link-home {
     margin-top: 80px;
   }
 }
