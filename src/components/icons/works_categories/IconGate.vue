164d4a342cf2e711fdb6ab941151dc372d750dc7
Oleg Andreev <olegandreev1997@mail.ru>
2024-09-30 15:32:41 +0300

    Add icon components for work categories

---
+++ src/components/icons/works_categories/IconGate.vue
@@ -0,0 +1,57 @@
+<template>
+  <svg
+    version="1.1"
+    id="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 128 128"
+    xml:space="preserve"
+    fill="currentColor"
+    stroke="currentColor"
+  >
+    <g>
+      <g>
+        <path
+          class="st0"
+          d="M96.5,23.09v94.89h-65V23.09c3.57-3.24,15.55-13.31,33-13.11C81.34,10.17,92.87,19.78,96.5,23.09z"
+        />
+        <line class="st0" x1="31" y1="46.93" x2="97" y2="46.93" />
+        <line class="st0" x1="31" y1="97.08" x2="97" y2="97.08" />
+        <line class="st0" x1="80.5" y1="117.85" x2="80.5" y2="12.96" />
+        <line class="st0" x1="64" y1="117.85" x2="64" y2="10.57" />
+        <line class="st0" x1="47.5" y1="117.85" x2="47.5" y2="12.96" />
+      </g>
+      <g>
+        <path
+          d="M4.55,33.02h11.89c2.51,0,4.55,2.04,4.55,4.55v79.89c0,2.51-2.04,4.55-4.55,4.55H4.55c-2.51,0-4.55-2.04-4.55-4.55V37.58
+			C0,35.06,2.04,33.02,4.55,33.02z"
+        />
+        <circle cx="10.5" cy="23.52" r="9.5" />
+      </g>
+      <g>
+        <path
+          d="M111.55,32.93h11.89c2.51,0,4.55,2.04,4.55,4.55v79.89c0,2.51-2.04,4.55-4.55,4.55h-11.89c-2.51,0-4.55-2.04-4.55-4.55
+			V37.48C107,34.97,109.04,32.93,111.55,32.93z"
+        />
+        <circle cx="117.5" cy="23.43" r="9.5" />
+      </g>
+    </g>
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+svg {
+  height: 24px;
+  width: 24px;
+}
+
+.st0 {
+  fill: none;
+  stroke: currentColor;
+  stroke-width: 7px;
+  stroke-linejoin: round;
+  stroke-miterlimit: 10px;
+}
+</style>
---
+++ src/components/icons/works_categories/IconMarquises.vue
@@ -0,0 +1,54 @@
+<template>
+  <svg
+    version="1.1"
+    id="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 128 128"
+    xml:space="preserve"
+    fill="currentColor"
+    stroke="currentColor"
+  >
+    <g>
+      <rect x="42.86" y="48.61" class="st0" width="66.72" height="66.72" />
+      <polygon class="st0" points="76.22,9.29 30.95,48.61 121.5,48.61 	" />
+      <polyline
+        class="st1"
+        points="42.86,60.45 9.5,60.45 9.5,75.35 42.86,75.35 	"
+      />
+      <path d="M23.5,118.71h19.36v-13.11H25.5c-1.1,0-2,0.9-2,2V118.71z" />
+      <path d="M6.5,118.71h19.68v-7.15H8.5c-1.1,0-2,0.9-2,2V118.71z" />
+      <line class="st1" x1="17.46" y1="110.56" x2="17.46" y2="75.35" />
+      <line class="st1" x1="33.14" y1="110.56" x2="33.14" y2="75.35" />
+      <rect x="71.46" y="64.1" class="st1" width="25.02" height="25.02" />
+      <line class="st1" x1="83.97" y1="64.1" x2="83.97" y2="89.12" />
+    </g>
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+svg {
+  height: 24px;
+  width: 24px;
+}
+
+.st0 {
+  fill: none;
+  stroke: currentColor;
+  stroke-width: 8px;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-miterlimit: 10px;
+}
+
+.st1 {
+  fill: none;
+  stroke: currentColor;
+  stroke-width: 6px;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-miterlimit: 10px;
+}
+</style>
